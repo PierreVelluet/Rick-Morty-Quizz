728ca5ca5cb7c6588695fb95025d822fc1ecaad7
PierreVelluet <velluetp@gmail.com>
2020-05-23 12:12:15 +0200

    job done

---
+++ quizz.js
@@ -0,0 +1,251 @@
+const data = {
+
+    1 : {
+        question: "Dans la saison 3, pour éviter une séance chez le psy, en quoi Rick se transforme-t-il ?",
+        choice1: "SALAD RICK !!!",
+        choice2: "TOMATO RICK !!!",
+        choice3: "CUCUMBER RICK !!!",
+        choice4: "PICKLE RICK !!!",
+        answer: "PICKLE RICK !!!",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm-1.png",
+        },
+    
+    2 : {
+        question: "Quelle est la particularité des voix originales de Rick et de Morty ?",
+        choice1: "Elles ont été créées par ordinateur",
+        choice2: "C’est une femme qui les interprète",
+        choice3: "C’est un enfant et son grand-père dans la vraie vie",
+        choice4: "Elles sont interprétées par la même personne",
+        answer: "Elles sont interprétées par la même personne",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm2.jpg",
+        },
+    
+    3 : {
+        question: "Pour sauver la Terre, Rick et Morty font appel à un célèbre rappeur. Qui est-ce ?",
+        choice1: "Dr Dre",
+        choice2: "Booba",
+        choice3: "Ice-T",
+        choice4: "Eminem",
+        answer: "Ice-T",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm-39.png",
+        },
+    
+    4 : {
+        question: "Dans la saison 3, on apprend que les extra-terrestres connaissent les humains de la Terre pour ...",
+        choice1: "Leur étonnante sexualité",
+        choice2: "Leur sens métaphysique",
+        choice3: "Leur fascination pour les pénis",
+        choice4: "Leur gastronomie",
+        answer: "Leur fascination pour les pénis",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm333.jpg",
+        },
+    
+    5: {
+        question: "Dans l’émission intergalactique Les aventures de Stealy, combien coûte un Plumbus ?",
+        choice1: "6,5 brapples",
+        choice2: "1 bitcoin",
+        choice3: "100 dollars",
+        choice4: "15,5 brapples",
+        answer: "6,5 brapples",
+        image: "https://www.numerama.com/content/uploads/2018/05/nfhgehr.jpg",
+    },
+    
+    6: {
+        question: "Dans la première saison, comment s’appelle le jeu en VR où Morty incarne un homme de sa naissance à sa mort ?",
+        choice1: "Charlie",
+        choice2: "Roy",
+        choice3: "Troy",
+        choice4: "Ben",
+        answer: "Roy",
+        image: "https://www.numerama.com/content/uploads/2018/05/jfizejf.jpg",
+        },
+        
+    7: {
+        question: "Quel est l’autre pouvoir de « Fart », le nuage constitué de gaz qui communique par télépathie et qui chante le célèbre « Goodbye Moonmen » ?",
+        choice1: "Il peut changer la composition de tous les atomes",
+        choice2: "Il fait péter n’importe quelle personne qu’il frôle",
+        choice3: "Il prend la forme de la personne avec qui il parle",
+        choice4: "Il peut se téléporter dans toutes les réalités à tout moment ",
+        answer: "Il peut changer la composition de tous les atomes",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm.png",
+        },
+    8: {
+        question: "Dans « Meeseeks and Destroy », les Mr. Meeseeks déraillent et se multiplient parce qu’ils n’arrivent pas à répondre à une demande : laquelle ?",
+        choice1: "Être une personne plus accomplie",
+        choice2: "S’améliorer au golf",
+        choice3: "Avoir un plus grand pénis",
+        choice4: "Être plus populaire à l’école",
+        answer: "S’améliorer au golf",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm-2.png",
+        },
+    9: {
+        question: "Dans « Total Rickall », des personnages s’invitent dans la mémoire de la famille alors qu’ils n’existent pas. Lequel de ces personnages n’apparaît pas dans l’épisode?",
+        choice1: "Mrs Chandeliere, le chandelier apprêté",
+        choice2: "Steve, le frère de Jerry",
+        choice3: "Mrs Refrigerator, le frigo sympa",
+        choice4: "Mr. Beauregard, le majordome pédant",
+        answer: "Mrs Chandeliere, le chandelier apprêté",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm.jpeg",
+        },
+    10: {
+        question: "Morty a seulement 14 ans, mais il est …",
+        choice1: "Extrêmement âgé selon le temps d’une autre planète",
+        choice2: "Alcoolique",
+        choice3: "Père de famille",
+        choice4: "Un super-héros",
+        answer: "Père de famille",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm-1.jpg",
+        },
+    11: {
+        question: "Rick a une amante très spéciale. Pourquoi ?",
+        choice1: "C’est un esprit de ruche",
+        choice2: "C’est un programme informatique",
+        choice3: "C’est un virus",
+        choice4: "C'est un robot",
+        answer: "C’est un esprit de ruche",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm-2.jpg",
+        },
+    12: {
+        question: "Dans le mariage qui clôt la saison 2, qui sont les heureux mariés ?",
+        choice1: "Diablo Verde et Arthricia",
+        choice2: "Rick et Squanchy",
+        choice3: "Birdperson et Tammy",
+        choice4: "Beth et un Mr. Meeseeks",
+        answer: "Birdperson et Tammy",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm.jpg",
+        },
+    13: {
+        question: "Morty a régulièrement demandé à son grand-père d’effacer ses souvenirs. Lequel de cette liste n’en fait pas partie ?",
+        choice1: "Il s’est aussi marié avec Birdperson",
+        choice2: "Il fait douter un Floop Floopian de sa religion",
+        choice3: "Il a tué 10 personnes en appuyant sur le mauvais interrupteur",
+        choice4: "Il croyait qu’il y avait un homme sur la lune",
+        answer: "Il s’est aussi marié avec Birdperson",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm-4.png",
+        },
+    14: {
+        question: "La voiture de Rick fonctionne avec l’effort d’une civilisation contenue dans son moteur. Comment s’appelle son président ?",
+        choice1: "Chris",
+        choice2: "Mini Rick",
+        choice3: "Chad",
+        choice4: "John",
+        answer: "Chris",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm-5.png",
+        },
+    15: {
+        question: "Dans « A Rickle in Time », une dispute entre Rick et Summer crée une séparation entre deux univers presque identiques. Ils ne sont à présent dans aucun des deux espaces-temps. Rick le prouve en leur montrant :",
+        choice1: "Qu’à chaque fois qu’ils parlent une parole sur deux est un miaulement ",
+        choice2: "Que leur maison a été transporté dans un Néant, dans lequel flottent des chats de Schrödinger.",
+        choice3: "Que leur chien s’est transformé en chat de Schrödinger",
+        choice4: "Qu’ils sont en train de se transformer en chats de Schrödinger",
+        answer: "Que leur maison a été transporté dans un Néant, dans lequel flottent des chats de Schrödinger.",
+        image: "https://www.numerama.com/content/uploads/2018/05/rm-6.png",
+        },
+    16: {
+        question: "Quelle phrase n’a pas été prononcée par Rick ?",
+        choice1: "Personne n’existe volontairement, personne n’appartient à un endroit, tout le monde va mourir.",
+        choice2: "À mon plus grand accomplissement à ce jour : m’ouvrir aux autres. ",
+        choice3: "L’école n’est pas faite pour les gens intelligents.",
+        choice4: "Ne vous approchez pas de mon espace vital.",
+        answer: "Ne vous approchez pas de mon espace vital.",
+        image: "https://www.numerama.com/content/uploads/2018/05/maxresdefault-1.jpg",
+        },
+    17: {
+        question: "It's time to ...",
+        choice1: "get dressed !",
+        choice2: "get lucky !",
+        choice3: "get schwifty !",
+        choice4: "get shclurmy !",
+        answer: "get schwifty !",
+        image: "https://www.numerama.com/content/uploads/2018/05/maxresdefault-1-1.jpg",
+        },
+    
+    };
+    const questionPassed = [];
+    const questionDiv = document.getElementById("questionDiv");
+    const answers = document.getElementsByTagName('td');
+    const actualScore = document.getElementById("score");
+    let randomData = 0;
+    
+    randomQuestion()
+    addEvent()
+    
+    function checkAnswer(clickedEvent) {
+    
+        if( clickedEvent.target.innerHTML === data[randomData].answer) {
+            clickedEvent.target.classList.add("rightAnswer")
+            actualScore.innerHTML = parseInt(actualScore.innerHTML) +1
+            document.getElementById("rightBubble").style.opacity = 1;
+        }else{
+            clickedEvent.target.classList.add("wrongAnswer")
+            for (let i=0 ; i<answers.length; i++) {
+                if (answers[i].innerHTML === data[randomData].answer) {
+                    answers[i].classList.add("rightAnswer")
+                }
+            }
+            document.getElementById("wrongBubble").style.opacity = 1;
+        };
+            countdown();
+            deleteEvent();
+            questionDiv.style.opacity = 0;
+    };
+    function randomQuestion() {
+            questionDiv.style.opacity = 1;
+            document.getElementById("rightBubble").style.opacity = 0;
+            document.getElementById("wrongBubble").style.opacity = 0;
+    let random = Math.floor(Math.random() * Object.keys(data).length) + 1;
+        if (questionPassed.indexOf(random) == -1) {
+            removeAllClass();
+            document.getElementById("questionDiv").innerHTML = data[random].question;
+            document.getElementById("choiceOne").innerHTML = data[random].choice1;
+            document.getElementById("choiceTwo").innerHTML = data[random].choice2;
+            document.getElementById("choiceThree").innerHTML = data[random].choice3;
+            document.getElementById("choiceFour").innerHTML = data[random].choice4;
+            document.getElementById("image").style.backgroundImage = "url(" + data[random].image +")";
+            randomData = random;
+            questionPassed.push(random);
+        }else if (questionPassed.length === Object.keys(data).length){
+            deleteEvent();
+            removeAllClass();
+            questionDiv.innerHTML = "GAME OVER !"
+            questionDiv.style.height = "30px";
+            questionDiv.style.width = "150px";
+            questionDiv.style.marginTop = "255px";
+            questionDiv.style.paddingBottom = "35px";
+            document.getElementById("table").style.opacity = "0";
+            document.getElementById("image").style.backgroundImage = "url(http://fr.web.img5.acsta.net/r_640_360/newsv7/19/11/07/18/44/3369432.jpg)";
+        }else {
+            randomQuestion();
+        }
+    };
+    function countdown() {
+    questionDiv.innerHTML = "";
+    let counter = 4;
+    let interval = setInterval(function() {
+        counter--;
+        document.getElementById("countdown").innerHTML = counter;
+        if (counter == 0) {
+            clearInterval(interval);
+            randomQuestion()
+            document.getElementById("countdown").innerHTML = "";
+            addEvent()
+        }
+    }, 1000);
+    }
+    function deleteEvent() {
+        for (let i=0; i<answers.length; i++) {
+            answers[i].removeEventListener("click", checkAnswer);
+        }
+        console.log('passed')
+    };
+    function addEvent() {
+        for (let i=0 ; i<answers.length; i++) {
+        answers[i].addEventListener('click', checkAnswer);  
+    }
+    }
+    function removeAllClass() {
+        for (let i=0; i<answers.length; i++) {
+            answers[i].classList.remove("rightAnswer");
+            answers[i].classList.remove("wrongAnswer");
+            };
+    }
